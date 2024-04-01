3afe46ee8c72721116b6869d0a78cb8f52f2b802
aselemidivine <aselemidivine@gmail.com>
2024-04-01 16:56:00 +0100

    test continues

--- src/intro.js
+++ src/intro.js
@@ -10,3 +10,17 @@ export function fizzBuzz(n) {
   if (n % 5 === 0) return 'Buzz';
   return n.toString(); // Convert the number (n) to a string.
 }
+
+
+export function calculateAverage(numbers) {
+  if(numbers.length === 0) return NaN; 
+
+  const sum = numbers.reduce((sum, current) => sum + current, 0);
+  return sum / numbers.length;
+}
+
+export function factorial(n) {
+  if(n < 0) return undefined
+  if(n === 0 || n === 1) return 1;
+  return n * factorial(n - 1); 
+}
--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect} from "vitest";
-import { fizzBuzz, max } from "../src/intro";
+import { fizzBuzz, max, calculateAverage } from "../src/intro";
 
 describe('max', () => {
     it('should return first argument if it is greater', () => {
@@ -30,4 +30,40 @@ describe('fizzBuzz', () => {
     it('should return the argument as a string if it is not divisible by 3 and 5', () => {
         expect(fizzBuzz(7)).toBe('7')
     })
+})
+
+describe('calculateAverage', () => {
+    it('should return NaN if given an empty array', () => {
+        expect(calculateAverage([])).toBe(NaN);
+    })
+    it('should calculate the average of an array with a single element', () => {
+        expect(calculateAverage([1])).toBe(1);
+    })
+    it('should calculate the average of an array with two elements', () => {
+        expect(calculateAverage([1, 2])).toBe(1.5);
+    })
+    it('should calculate the average of an array with three elements', () => {
+        expect(calculateAverage([1, 2, 3])).toBe(2);
+    })
+})
+
+describe('factorial', () => {
+    it('it should return 1 if given 0'), () => {
+        expect(factorial(0)).toBe(1);
+    } 
+    it('it should return 1 if given 1'), () => {
+        expect(factorial(1)).toBe(1);
+    } 
+    it('it should return 2 if given 2'), () => {
+        expect(factorial(2)).toBe(2);
+    } 
+    it('it should return 6 if given 3'), () => {
+        expect(factorial(3)).toBe(6);
+    } 
+    it('it should return 24 if given 4'), () => {
+        expect(factorial(4)).toBe(24);
+    } 
+    it('it should return undefined if given a negative number'), () => {
+        expect(factorial(-4)).toBeUndefined();
+    } 
 })
